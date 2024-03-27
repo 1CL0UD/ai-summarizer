17a52a6b1619c0ef8eb234f6175ef84712ffba1b
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-03-27 16:04:15 +0700

    url history section

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,24 +1,43 @@
 // import { useState, useEffect } from 'react';
-// import copy  from '../assets/copy.svg';
-import { useState } from 'react';
+import copy from '../assets/copy.svg';
+import { useEffect, useState } from 'react';
 import link from '../assets/link.svg';
 // import  loader  from '../assets/loader.svg';
 // import  tick  from '../assets/tick.svg';
 
 import { useLazyGetSummaryQuery } from '../services/article';
 
+interface Article {
+  url: string;
+  article: string;
+  summary?: string;
+}
+
 const Demo = () => {
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-  const [article, setArticle] = useState({ url: '', article: '' });
+  const [article, setArticle] = useState<Article>({ url: '', article: '' });
+  const [allArticles, setAllArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const articlesFromLocalStorage = localStorage.getItem('articles');
+    if (articlesFromLocalStorage !== null) {
+      const parsedArticles = JSON.parse(articlesFromLocalStorage);
+      setAllArticles(parsedArticles);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
-      console.log(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
@@ -46,7 +65,26 @@ const Demo = () => {
             ⏎
           </button>
         </form>
-        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              className="link_card"
+              onClick={() => setArticle(item)}
+              key={`link-${index}`}
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Results */}
     </section>
